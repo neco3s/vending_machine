eaa1fde95ab0dd340e62d15e5a55f3beef2bcdc1
neco3s <neco3coffee@gmail.com>
2023-05-07 21:26:08 +0900

    add: Suicaの仮実装

--- ruby/suica.rb
+++ ruby/suica.rb
@@ -1,3 +1,30 @@
 class Suica
+  attr_reader :balance
 
+  def initialize
+    @balance = 0
+    charge(500)
+  end
+
+  def charge(deposit)
+    if deposit > 100
+      @balance += deposit
+      "charge後の残高: #{@balance}"
+    else
+      "100¥未満はchargeできません"
+    end
+  end
+
+  def purchase(drink)
+    # TODO purchase(drink)の実装
+  end
+
+  def show_balance()
+    "現在の残高: #{@balance}"
+  end
 end
+
+my_suica = Suica.new
+my_suica.charge(1)
+p my_suica.show_balance
+p my_suica.balance
